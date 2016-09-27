34619b148156eb64e2327cd14b8efb4694db6bdc
jessemavi <jesse.mavi@gmail.com>
2016-09-26 20:43:01 -0700

    finish a few prompts in the category of bare minimum
    -[x] use proper escaping on user inputs
    -[x] set up refresh button to display newest messages
    -[x] allow users to create rooms and enter existing rooms
    -[x] allow friends to be 'friends' with other user and display friends differently

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,23 +1,47 @@
 // YOUR CODE HERE:
 $(document).ready(function() {
-  $('button').on('click', function() {
+  app.fetch();
+
+  $('#submitRoom').on('click', function() {
+    var newRoom = $('#newRoom').val();
+    app.renderRoom(newRoom);
+  });
+
+  $('#refresh').on('click', function() {
+    app.clearMessages();
+    app.fetch();
+  });
+
+  $('#send').on('submit', function() {
+    event.preventDefault();
     var username = document.URL.slice(_.lastIndexOf(document.URL, '=') + 1);
-    var messageContent = $('#messageContent').val();
-    var roomChoice = document.getElementById('roomChoice');
-    var roomSelected = roomChoice.options[roomChoice.selectedIndex].text;
+    var messageContent = $('#message').val();
+    var roomSelect = document.getElementById('roomSelect');
+    var roomSelected = roomSelect.options[roomSelect.selectedIndex].text;
     var message = {
       username: username,
       text: messageContent,
       roomname: roomSelected
     };
     app.send(message);
-    // var room = $("#roomChoice option:selected").text();
-    // console.log(room);
   });
+
+  $(document).on('click', '.tweetUser', function() {
+    if (_.indexOf(app.friends, $(this).text()) === -1) {
+      app.friends.push($(this).text());
+    }
+    app.clearMessages();
+    app.fetch();
+  });
+
 });
+
+
 
 const app = {};
 app.server = 'https://api.parse.com/1/classes/messages';
+app.friends = [];
+
 app.init = function() {
 
 };
@@ -45,13 +69,65 @@ app.fetch = function () {
     url: app.server,
     type: 'GET',
     contentType: 'application/json',
+    data: {limit: 100, order: '-createdAt'},
     success: function (data) {
-      console.log(data);
+      // 'order=-updatedAt';
+      _.each(data.results, function(datum) {
+        app.renderMessage(datum);
+      });
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message', data);
     }
-});
+  });
 
 };
+
+app.clearMessages = function() {
+  // var chatsNode = document.getElementById('#chats');
+  // while (chatsNode.firstChild) {
+  //   console.log(chatsNode);
+  //   chatsNode.removeChild(chatsNode.firstChild);
+  // }
+  $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  var tweet;
+  var user = $('<a class="tweetUser" href="#"></a>').text(message.username);
+  var time = $('<span class="tweetTime"></span>').text(message.updatedAt);
+  var msg = $('<p class="tweetMessage"></p>').text(message.text);
+  if ((_.indexOf(app.friends, message.username) === -1)) {
+    $tweets = $('<div class="tweet"></div>').append(user).append(time).append(msg);
+  } else {
+    $tweets = $('<div class="tweet friend"></div>').append(user).append(time).append(msg);
+  }
+  $('#chats').append($tweets);
+};
+
+app.renderRoom = function(roomName) {
+  var childLength = $('#roomSelect').children().length;
+  var newRoom = '<option value = "' + (childLength + 1) + '">' + roomName + '</option>';
+  var $newRoom = $(newRoom);
+  $('#roomSelect').append($newRoom);
+};
+
+
+
+  // $('button').on('click', function() {
+  //   var username = document.URL.slice(_.lastIndexOf(document.URL, '=') + 1);
+  //   var messageContent = $('#messageContent').val();
+  //   var roomChoice = document.getElementById('roomChoice');
+  //   var roomSelected = roomChoice.options[roomChoice.selectedIndex].text;
+  //   var message = {
+  //     username: username,
+  //     text: messageContent,
+  //     roomname: roomSelected
+  //   };
+  //   app.send(message);
+  // });
+
+
+
+
